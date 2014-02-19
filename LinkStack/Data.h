7198377d79e794ae9982be38b941f4d235dcc642
QingDengKuZhu <2583746755@qq.com>
2014-02-19 22:51:05 +0800

    链式栈相关数据结构定义以及支持的基本操作

---
+++ LinkStack/Data.h
@@ -0,0 +1,32 @@
+﻿/*****************
+**栈的链式数据结构
+*****************/
+#ifndef DATA_H
+#define DATA_H
+
+#include "Main_First.h"
+
+typedef int Elem;
+
+/*
+**结点数据结构.
+*/
+typedef struct Node
+{
+	Elem data;			/*数据域.*/
+	struct Node *pNext;	/*指针域.*/
+
+}NODE, *PNODE;
+
+/*
+**链式栈的数据结构.
+*/
+typedef struct LinkStack
+{
+	PNODE *pBottom;			/*栈底指针.*/
+	PNODE *pTop;			/*栈顶指针,始终指向栈顶元素的下一个位置.*/
+
+}LINKSTACK, *PLINKSTACK;
+
+
+#endif
