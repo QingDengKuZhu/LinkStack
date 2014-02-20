efaf8762e3d33266796d27052e63f8d1405e8de7
QingDengKuZhu <2583746755@qq.com>
2014-02-20 13:48:35 +0800

    修改Push原型

--- LinkStack/Data_Base.h
+++ LinkStack/Data_Base.h
@@ -28,8 +28,8 @@ size_t StackLength(PLINKSTACK pS);
 /*获取栈顶元素.若操作成功,函数返回OK,*e保存栈顶的值;若操作失败,函数返回FAILE,*e为垃圾值.*/
 STATUS GetTop(PLINKSTACK pS, Elem *e);
 
-/*插入新的数据元素v为栈顶元素.若成功,函数返回OK;否者返回FAILE.*/
-STATUS Push(PLINKSTACK pS, const Elem v);
+/*插入新的数据元素v为栈顶元素.*/
+void Push(PLINKSTACK pS, const Elem v);
 
 /*删除栈顶元素.若成功删除,函数返回OK,*e保存被删除元素的值;否者函数返回FAILE,*e为垃圾值.*/
 STATUS Pop(PLINKSTACK pS, Elem *e);
