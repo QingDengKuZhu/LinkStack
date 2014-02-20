99c4b70b524bcebd47ad686622dd8021e50180a0
QingDengKuZhu <2583746755@qq.com>
2014-02-20 14:08:46 +0800

    Bug
    数据结构定义错误

--- LinkStack/Data.h
+++ LinkStack/Data.h
@@ -23,8 +23,8 @@ typedef struct Node
 */
 typedef struct LinkStack
 {
-	PNODE *pBottom;			/*栈底指针.*/
-	PNODE *pTop;			/*栈顶指针,始终指向栈顶元素的下一个位置.*/
+	PNODE pBottom;			/*栈底指针.*/
+	PNODE pTop;			/*栈顶指针,始终指向栈顶元素的下一个位置.*/
 
 }LINKSTACK, *PLINKSTACK;
 
